982277e8568c6d3af8c14a0c75b5f484f54697a1
Sylvian-Brunet <sylvian84@hotmail.fr>
2020-01-10 15:27:03 +0100

    V1.2.1 - localstorage

--- common/api.js
+++ common/api.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 
 export async function asyncGetProducts() {
     try {
+        const storedProducts = localStorage.getItem('storedProducts');
+        if (storedProducts) {
+          console.log('Stored version found');
+          return JSON.parse(storedProducts);
+        }
         const response = await axios.get('http://localhost:3001');
         const { data } = response;
+
+        localStorage.setItem('storedProducts', JSON.stringify(data));
         return data;
     } catch (err) {
         console.error('o no');
